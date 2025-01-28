9b35b7b592232bb0ac52041ed5045c1c7f2f4425
Jordan Price <rj_price@hotmail.co.uk>
2025-01-28 10:17:51 +0000

    add tabs, tidy up README

--- README.md
+++ README.md
@@ -1,28 +1,64 @@
 # EDA Dashboard
-A Streamlit dashboard for exploratory data analysis (EDA)
 
-## Set up environment
+### Overview
+`eda_dashboard` is a Streamlit dashboard app for rapid exploratory data analysis (EDA) and visualisation of tabular numerical data in `.csv` format.
+
+### Features
+The app provides the following functionality:
+- View data, summary statistics and number of missing values per column.
+- Generate a correlation matrix and heatmap.
+- Visualise data distributions as histograms.
+- Plot selected column as bar chart.
+- Determine pairwise relationships between specified columns via scatterplots.
+
+<br>
+
+## Getting Started
+
+### Requirements
+The following software packages must be installed prior to running (please refer to the linked installation instructions for your operating system):
+- [Git](https://github.com/git-guides/install-git)
+- [Python](https://www.python.org/downloads/)
+
+### Installation
+After installing Git and Python, install the app as follows:
 ```bash
+# Get app from GitHub
+git clone https://github.com/rj-price/eda_dashboard.git 
+
+# Change into directory
+cd eda_dashboard
+
+# Create Python virtual environment 
 python -m venv venv
+
+# Activate environment
 source venv/bin/activate
+
+# Install required Python packages
 pip install -r requirements.txt
+
+# Run EDA Dashboard:
+streamlit run eda_dashboard.py
 ```
 
-## Dashboard
+## Dashboard Use
 Set up initial dashboard using `assemblies.csv` containing genome assembly metrics.
 
 Initial dashboard can plot barcharts or scatterplots based on any of the column headers, selected through the dropdown menus.
 
+<br>
+
+---
+---
+
+<br>
 
 ## TO DO
+### Features to add:
 - Upload button
 - Data types
 - Dataframe size
 - PCA
 - Clustering analysis
-
-### DONE
-- Missing data
-- Summary stats
-- Correlation plot
-- Histograms
+- Heatmap
--- eda_dashboard.py
+++ eda_dashboard.py
@@ -57,34 +57,41 @@ if show_missing:
 # If wanting to include additional data on barchart or scatterplot
 # Need conditionals, or buttons (ie. for barplot, button 1 ar, button for 2 bars, button for 3)
 
+st.markdown("<br>", unsafe_allow_html=True)
+
+# Create tabs for different visualizations
+tab1, tab2, tab3, tab4 = st.tabs(["Correlation Analysis", "Distribution Analysis", "Bar Charts", "Scatter Plots"])
+
 ####### Correlation #######
-st.write("## Correlation Matrix and Heatmap")
-if st.button("Show Correlation"):
-    st.write(data.corr())
-    corr_plot = sns.heatmap(data.corr(), annot=True)
-    st.pyplot(corr_plot.get_figure())
+with tab1:
+    st.write("## Correlation Matrix and Heatmap")
+    if st.button("Show Correlation"):
+        st.dataframe(data.corr(), use_container_width=True)
+        corr_plot = sns.heatmap(data.corr(), annot=True)
+        st.pyplot(corr_plot.get_figure())
 
 ####### Histogram #######
-st.write("## Histogram")
-hist_column = st.selectbox("Choose which data to plot as histogram:", headers)
-if st.button("Plot Histogram"):
-    hist_plot = sns.histplot(data=data, x=hist_column)
-    st.pyplot(hist_plot.get_figure())
+with tab2:
+    st.write("## Histogram")
+    hist_column = st.selectbox("Choose which data to plot as histogram:", headers)
+    if st.button("Plot Histogram"):
+        hist_plot = sns.histplot(data=data, x=hist_column)
+        st.pyplot(hist_plot.get_figure())
 
 ####### Barchart #######
-st.write("## Barchart")
-bar_column = st.selectbox("Choose which data to plot as barchart:", headers)
-
-if st.button("Plot Barchart"):
-    st.bar_chart(data[bar_column])
+with tab3:
+    st.write("## Barchart")
+    bar_column = st.selectbox("Choose which data to plot as barchart:", headers)
+    if st.button("Plot Barchart"):
+        st.bar_chart(data[bar_column])
     
 ####### Scatterplot #######
-st.write("## Scatterplot")
-scatter_x = st.selectbox("Choose which data to plot on X axis:", headers)
-scatter_y = st.selectbox("Choose which data to plot on Y axis:", headers)
-
-if st.button("Plot Scatterplot"):
-    st.scatter_chart(data=data, x=scatter_x, y=scatter_y)
+with tab4:
+    st.write("## Scatterplot")
+    scatter_x = st.selectbox("Choose which data to plot on X axis:", headers)
+    scatter_y = st.selectbox("Choose which data to plot on Y axis:", headers)
+    if st.button("Plot Scatterplot"):
+        st.scatter_chart(data=data, x=scatter_x, y=scatter_y)
     
 
 # Add footer
